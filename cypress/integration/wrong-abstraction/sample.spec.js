4b48dfce750f531428de94e8368abb2e46d5cf6b
gustavo alef <gustalef@gmail.com>
2022-06-06 17:22:14 -0300

    aula wrong-abstraction - refactor

--- cypress/integration/wrong-abstraction/sample.spec.js
+++ cypress/integration/wrong-abstraction/sample.spec.js
@@ -1,3 +1,4 @@
+/// <reference types="Cypress"/>
 describe('Wrong abstraction bad practice', () => {
   beforeEach(() => {
     cy.intercept(
@@ -13,20 +14,22 @@ describe('Wrong abstraction bad practice', () => {
     cy.search('cypress')
     cy.wait('@getStories')
 
-    cy.assertResults()
+    cy.assertResults() //<-- nao colocar assertions em custom command
   })
 
   it('uses custom command for assertion just for the sake of reusability', () => {
     cy.search('selenium')
     cy.wait('@getStories')
 
-    cy.assertResults()
+    cy.get(".table-row").then((rows) => {
+      expect(rows.length).to.be.at.least(1); //<-- assertion explicita
+    });
   })
 
   it('uses custom command for assertion just for the sake of reusability', () => {
     cy.search('playwright')
     cy.wait('@getStories')
 
-    cy.assertResults()
+    cy.get('.table-row').its('length').should('be.at.least', 1) //<-- assertion implicita
   })
 })
